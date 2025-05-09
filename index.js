5f09b1e4240dc53c3b6ade9ffce93b6d1eedd9d7
BerissaMuyizere <muyizereberissa@gmail.com>
2025-05-09 11:34:18 +0300

    feature toggle

---
+++ index.js
@@ -0,0 +1,166 @@
+function FeatureToggle(featureName, isEnabled, userGroupAccess){
+    this.featureName = featureName;
+    this.isEnabled = isEnabled;
+    this.userGroupAccess;
+    this.userGroupAccess = userGroupAccess
+}
+
+FeatureToggle.prototype.canAccess = function(userRole){
+    if(!this.isEnabled){
+        return false;
+    }
+    switch(userRole){
+        case "admin":
+            console.log(`You have ${userRole} access`);
+            break;
+        case "betaTester":
+            console.log(`You have ${userRole} access`);
+            break;
+        case "user":
+            console.log(`You have ${userRole} access`);
+            break;
+        case "guest":
+            console.log(`You have ${userRole} access`);
+            break;
+        default:
+            "Access not found"
+
+    }
+}
+FeatureToggle.prototype.toggleFeature = function(flag){
+    if(flag == "disabled"){
+        return flag = "enabled"
+    }
+    else{
+        return  flag = "disabled"
+    }
+};
+
+const feature = new FeatureToggle("darkmode", true, ["admin", "betaTester", "guest", "user"])
+
+feature.canAccess("admin")
+
+console.log(feature.toggleFeature("enabled"))
+
+
+
+
+
+
+function TimeLog(freelanceName, projectDetails, logs){
+    this.freelanceName = freelanceName;
+    this.projectDetails = projectDetails;
+    this.logs = logs
+}
+
+TimeLog.prototype.totalEarning = function(){
+    return this.logs.reduce((total, log) =>
+        total + (log.hoursWorked * this.projectDetails.hourlyRate), 0
+    )
+};
+
+TimeLog.prototype.filterLogsByDate = function(startDate, endDate){
+    return this.logs.filter(log =>
+        log.date >= startDate && log.date<= endDate
+    )
+};
+
+TimeLog.prototype.exceedsWeeklyHours = function(weekStartDate){
+    const weekLogs = this.filterLogsByDate(weekStartDate, new Date(weekStartDate.getTime() + 6 *24*60*60*1000))
+    const totalHuors = weekLogs.reduce((sum, log) => sum + log.hoursWorked, 0);
+    return totalHuors > 40;
+};
+
+const freelancer = new TimeLog("Jimmy", {name: "Deliveries", hourlyRate: 50}, [{date: new Date("2022-03-20"), hoursWorked: 5}, {date: new Date("2025-03-25"), hoursWorked: 5}, {date: new Date("2025-03-20"), hoursWorked: 5}])
+
+console.log(freelancer.totalEarning())
+freelancer.filterLogsByDate()
+
+
+
+
+
+// function Order(customer, items, status){
+//     this.customer = customer;
+//     this.items = items;
+//     this.status = status
+// }
+
+// Order.prototype.totalCost = function(){
+//     return this.items.reduce((total, item) => total + (item.quantity * item.unitPrice), 0);
+// };
+
+// Order.prototype.updateStatus = function(payment){
+//     this.status = payment ? "completed" : "pending";
+// };
+//  Order.prototype.checkUrgency = function(){
+//     const total = this.totalCost();
+//     if(total > 2000){
+//         return "high"
+//     }
+//     else if(total > 500){
+//         return "medium"
+//     }
+//     else{
+//         return "low"
+//     }
+//  };
+
+
+
+
+
+
+
+
+//  function Employee(id, name, performanceMetrics, feedback){
+//     this.id = id;
+//     this.name = name;
+//     this.performanceMetrics = performanceMetrics;
+//     this.feedback = feedback;
+//  }
+
+//  Employee.prototype.averageScore = function(){
+//     const scores = Object.values(this.performanceMetrics);
+//     return scores.reduce((sum, score) => sum + score, 0)/scores.length
+//  };
+
+//  Employee.prototype.classifyPerformance = function(){
+//     const avg = this.averageScore();
+//     if(avg >= 4.5){
+//         return "excelent"
+//     }
+//     else if(avg >= 3.5){
+//         return "good keep"
+//     }
+//     else{
+//         "improve"
+//     }
+//  }
+
+//  Employee.prototype.feedBack = function(newFeedback){
+//     if(newFeedback.trim().length > 0){
+//         this.feedBack.push(newFeedback)
+//     }
+//  }
+
+
+
+// function Course(title, instructor, students){
+//     this.title = title;
+//     this.instructor = instructor;
+//     this.students = students
+// }
+
+// Course.prototype.completedStudents = function(){
+//     return this.students.filter(student => student.completionStatus).map(student.name);
+// };
+
+// Course.prototype.enrolledStudentsByExpertiise = function(){
+//     return this.students.filter(student => student.expertise === this.instructor.expertise).length;
+
+// };
+
+// Course.prototype.instructorMessage = function(){
+//     return this.students.length > 5 ? "High demand instructor" : "Normal workload instructor"
+// }
